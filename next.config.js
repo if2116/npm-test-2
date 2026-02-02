cec8aa12a4725ca047b1038b20156f941d057726
dianshan <dianshan102@gmail.com>
2026-02-02 22:45:16 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
-const createNextIntlPlugin = require('next-intl/plugin');
-
-// Use i18n config from root directory
-const withNextIntl = createNextIntlPlugin('./i18n.ts');
-
 const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? 'npm-test-2';
 const isGhPages = process.env.GITHUB_ACTIONS === 'true';
 
@@ -17,4 +12,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withNextIntl(nextConfig);
+module.exports = nextConfig;
