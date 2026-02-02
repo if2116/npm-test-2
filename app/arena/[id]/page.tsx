46371efa0fd26086823a6a845dcdd7c324d43347
dianshan102 <dianshan102@gmail.com>
2026-02-02 23:49:31 +0800

    update

--- app/arena/[id]/page.tsx
+++ app/arena/[id]/page.tsx
@@ -1,4 +1,12 @@
 import { LocaleRedirect } from '@/components/locale-redirect';
+import { arenas } from '@/lib/data';
+
+export const dynamic = 'force-static';
+export const dynamicParams = false;
+
+export function generateStaticParams() {
+  return arenas.map((arena) => ({ id: arena.folderId }));
+}
 
 export default async function ArenaDetailRedirectPage({
   params,
